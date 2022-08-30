c0b145af206dad160ca820713a3ac0ae4d8996bc
Cofica17 <63316032+Cofica17@users.noreply.github.com>
2022-08-30 15:14:19 +0200

    addec clock syncronization

--- Server.gd
+++ Server.gd
@@ -34,10 +34,20 @@ remote func receive_player_transform(data):
 remote func receive_player_animation(anim):
 	movement.receive_player_animation(anim)
 
+remote func send_server_time(client_time):
+	var id = get_tree().get_rpc_sender_id()
+	rpc_id(id, "receive_server_time", OS.get_system_time_msecs(), client_time)
+
+remote func determine_latency(client_time):
+	var id = get_tree().get_rpc_sender_id()
+	rpc_id(id, "return_latency", client_time)
 
 ########################
 ### FOR SENDING INFO ###
 ########################
+
+func send_ping(player_id):
+	rpc_id(player_id, "receive_ping")
 
 func send_world_state(world_state):
 	rpc_unreliable_id(0, "receive_world_state", world_state)
